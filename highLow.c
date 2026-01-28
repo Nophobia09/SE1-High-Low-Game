4f28e0844679f4deeb42cc52348c109c2c0767fc
Nophobia09 <71725560+Nophobia09@users.noreply.github.com>
2026-01-28 17:36:50 -0500

    Initial commit

---
+++ highLow.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Welcome to High Low Game!\n");
+    int numberToGuess = rand() % 10 + 1; // Random number between 1 and 10
+    int currentGuess;
+    int attempts = 0;
+    const int maxAttempts = 5;
+    const int maxMistakes = 3;
+    int mistakes = 0;   
+    int previousGuess = 0; 
+    int guessedToLow = 0;
+    int guessedToHigh = 0;
+    int maxPlays = 0;
+
+    while (attempts < maxAttempts && mistakes < maxMistakes) {
+        printf("Enter your guess (1-10): ");
+        scanf("%d", &currentGuess);
+
+        if (currentGuess < 1 || currentGuess > 10) {
+            printf("Please guess a number between 1 and 10.\n");
+            mistakes++;
+            continue;
+        }
+        
+        if (previousGuess == currentGuess || currentGuess < previousGuess && guessedToLow || currentGuess > previousGuess && guessedToHigh) {
+            mistakes++;
+        } else {
+            previousGuess = currentGuess;
+        }
+
+        if (currentGuess < numberToGuess) {
+            printf("Higher!\n");
+            guessedToLow = 1;
+            guessedToHigh = 0;
+            attempts++;
+        } else if (currentGuess > numberToGuess) {
+            printf("Lower!\n");
+            guessedToHigh = 1;
+            guessedToLow = 0;
+            attempts++;
+        } else {
+            attempts++;
+            printf("Congratulations! You've guessed the number!\n");
+            printf("It took you %d attempts.\n", attempts);
+            if (maxPlays >= 3) {
+                printf("Thanks for Playing! You've reached the maximum number of plays.\n");
+                break;
+            }
+            printf("Game Over! You Win!\nWould you like to play again? (y/n): ");
+            char playAgain;
+            scanf(" %c", &playAgain);
+            if (playAgain == 'y' || playAgain == 'Y') {
+                numberToGuess = rand() % 10 + 1;
+                attempts = 0;
+                mistakes = 0;
+                previousGuess = 0;
+                guessedToLow = 0;
+                guessedToHigh = 0;
+                maxPlays ++;
+                continue;
+            } else {    
+            break;
+            }
+        }
+
+        if (mistakes >= maxMistakes) {
+            printf("Game Over! You've made too many mistakes. The number was %d.\n", numberToGuess);
+        } else if (attempts >= maxAttempts) {
+            printf("Game Over! You've used all your attempts. The number was %d.\n", numberToGuess);
+        }
+    }
+    return 0;
+}
